6e75f53f259061c467836dc80fea126ebfd1beee
Daniil Grishenok <grishenokdaniil5@gmail.com>
2024-08-23 16:53:57 +0300

    refactor: Бизнес логика вынесена из auth controller в свой слой

--- src/main/java/com/shsh/auth_service_social_network/controller/AuthController.java
+++ src/main/java/com/shsh/auth_service_social_network/controller/AuthController.java
@@ -27,58 +27,19 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final UserService userService;
-    private final JwtUtils jwtUtils;
-    private final AuthenticationManager authenticationManager;
-    private final KafkaProducer kafkaProducer;
+
     @PostMapping("/registration")
     public ResponseEntity<?> register(@RequestBody RegistrationUserDto registrationRequest) {
-        try {
-            if (!registrationRequest.getPassword().equals(registrationRequest.getConfirmPassword())) {
-                throw new PasswordMismatchException("Password and Confirm Password do not match");
-            }
-            try {
-                User user = new User();
-                user.setEmail(registrationRequest.getEmail());
-                user.setUsername(registrationRequest.getUsername());
-                user.setPassword(registrationRequest.getPassword());
-
-                userService.save(user);
-                UserRegistrationEvent event = new UserRegistrationEvent(user.getId(), user.getUsername(), user.getEmail());
-                kafkaProducer.sendMessage(event);
-            } catch (Exception e) {
-                log.error("Error saving user: {}", e.getMessage());
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                        .body(new AppError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "An error occurred while saving the user"));
-            }
-        } catch (PasswordMismatchException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(new AppError(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
-        }
-        return ResponseEntity.ok("User registered successfully");
+        return userService.registerNewUser(registrationRequest);
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody JwtRequest authRequest) {
-        try {
-            authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
-        } catch (BadCredentialsException e) {
-            log.error("Invalid login credentials for user: {}", authRequest.getUsername());
-            return new ResponseEntity<>(new AppError(HttpStatus.UNAUTHORIZED.value(), "Invalid credentials"), HttpStatus.UNAUTHORIZED);
-        }
-
-        UserDetails userDetails = userService.findByUsername(authRequest.getUsername());
-        User user = userService.findByUsername(authRequest.getUsername());
-        String token = jwtUtils.generateJwtToken(userDetails.getUsername(), user.getEmail(), String.valueOf(user.getId()));
-
-        return ResponseEntity.ok(new JwtResponse(token));
+        return userService.authenticateUser(authRequest);
     }
 
     @ExceptionHandler(PasswordMismatchException.class)
     public ResponseEntity<AppError> handlePasswordMismatchException(PasswordMismatchException ex) {
         return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(), ex.getMessage()), HttpStatus.BAD_REQUEST);
     }
-
-
-
 }
--- src/main/java/com/shsh/auth_service_social_network/service/UserService.java
+++ src/main/java/com/shsh/auth_service_social_network/service/UserService.java
@@ -1,20 +1,34 @@
 package com.shsh.auth_service_social_network.service;
 
+import com.shsh.auth_service_social_network.dto.JwtRequest;
+import com.shsh.auth_service_social_network.dto.JwtResponse;
+import com.shsh.auth_service_social_network.dto.RegistrationUserDto;
+import com.shsh.auth_service_social_network.dto.UserRegistrationEvent;
+import com.shsh.auth_service_social_network.exceptions.AppError;
+import com.shsh.auth_service_social_network.exceptions.PasswordMismatchException;
+import com.shsh.auth_service_social_network.security.JwtUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.shsh.auth_service_social_network.repository.UserRepository;
 import com.shsh.auth_service_social_network.model.User;
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final KafkaProducer kafkaProducer;
+    private final JwtUtils jwtUtils;
+    private final AuthenticationManager authenticationManager;
 
     public User findByUsername(String username) {
         return userRepository.findByUsername(username);
@@ -29,5 +43,46 @@ public class UserService {
         userRepository.save(user);
     }
 
+    public ResponseEntity<?> registerNewUser(RegistrationUserDto registrationRequest) {
+        try {
+            if (!registrationRequest.getPassword().equals(registrationRequest.getConfirmPassword())) {
+                throw new PasswordMismatchException("Password and Confirm Password do not match");
+            }
+            User user = new User();
+            user.setEmail(registrationRequest.getEmail());
+            user.setUsername(registrationRequest.getUsername());
+            user.setPassword(registrationRequest.getPassword());
 
+            save(user);
+
+            UserRegistrationEvent event = new UserRegistrationEvent(user.getId(), user.getUsername(), user.getEmail());
+            kafkaProducer.sendMessage(event);
+
+            return ResponseEntity.ok("User registered successfully");
+        } catch (PasswordMismatchException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new AppError(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
+        } catch (Exception e) {
+            log.error("Error saving user: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new AppError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "An error occurred while saving the user"));
+        }
+    }
+
+    public ResponseEntity<?> authenticateUser(JwtRequest authRequest) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        } catch (BadCredentialsException e) {
+            log.error("Invalid login credentials for user: {}", authRequest.getUsername());
+            return new ResponseEntity<>(new AppError(HttpStatus.UNAUTHORIZED.value(), "Invalid credentials"), HttpStatus.UNAUTHORIZED);
+        }
+
+        UserDetails userDetails = findByUsername(authRequest.getUsername());
+        User user = findByUsername(authRequest.getUsername());
+        String token = jwtUtils.generateJwtToken(userDetails.getUsername(), user.getEmail(), String.valueOf(user.getId()));
+
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
 }
+
